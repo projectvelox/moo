6acadacd341448b1acbf71e40ab4cca3618b7d13
projectvelox <43872288+projectvelox@users.noreply.github.com>
2020-05-06 01:43:08 +0800

    Update QnABot.cs

--- Bots/QnABot.cs
+++ Bots/QnABot.cs
@@ -66,7 +66,7 @@ namespace Microsoft.BotBuilderSamples.Bots
                 
                 //await turnContext.SendActivityAsync(Dialog.RunAsync(turnContext, ConversationState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken), cancellationToken);
 
-                await Dialog.RunAsync(turnContext.SendActivityAsync(replyActivity, cancellationToken), ConversationState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
+                await Dialog.RunAsync(turnContext, ConversationState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
 
             }
 
