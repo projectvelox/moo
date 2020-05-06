cc0f3e9b699342cc4c15ff009ef0daac2d77ce90
projectvelox <43872288+projectvelox@users.noreply.github.com>
2020-05-07 03:22:49 +0800

    added enpoint key

--- Bots/QnABot.cs
+++ Bots/QnABot.cs
@@ -51,7 +51,7 @@ namespace Microsoft.BotBuilderSamples.Bots
             var qnaMaker = new QnAMaker(new QnAMakerEndpoint
             {
                 KnowledgeBaseId = "bbb9cb8b-bef5-44b3-b3f0-c4fe30a4e63d",
-                EndpointKey = "68bddf3c-07d6-47cd-91a9-d49fc575ee7b",
+                EndpointKey = "EndpointKey 68bddf3c-07d6-47cd-91a9-d49fc575ee7b",
                 Host = "mooqnakb.azurewebsites.net"
             },
             null,
