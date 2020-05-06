bf08e01c106330f9218e3c340e827672c3e994fb
projectvelox <43872288+projectvelox@users.noreply.github.com>
2020-05-07 04:46:32 +0800

    Update QnABot.cs

--- Bots/QnABot.cs
+++ Bots/QnABot.cs
@@ -48,8 +48,8 @@ namespace Microsoft.BotBuilderSamples.Bots
             var qnaMaker = new QnAMaker(new QnAMakerEndpoint
             {
                 KnowledgeBaseId = "bbb9cb8b-bef5-44b3-b3f0-c4fe30a4e63d",
-                EndpointKey = "EndpointKey 68bddf3c-07d6-47cd-91a9-d49fc575ee7b",
-                Host = "mooqnakb.azurewebsites.net"
+                EndpointKey = "68bddf3c-07d6-47cd-91a9-d49fc575ee7b",
+                Host = "mooqnakb.azurewebsites.net/qnamaker"
             },
             null,
             httpClient);
