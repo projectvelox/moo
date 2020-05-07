c1cf6d3e7f135f5b1c07226a0c9433fb1343dd4c
projectvelox <43872288+projectvelox@users.noreply.github.com>
2020-05-08 06:52:47 +0800

    Update QnABot.cs

--- Bots/QnABot.cs
+++ Bots/QnABot.cs
@@ -1,14 +1,21 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+using System;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
 using System.Net.Http;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.AI.QnA;
+using Microsoft.Bot.Builder.AI.QnA.Dialogs;
+using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Schema;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-
 using EchoBot.OmniChannel;
 
 namespace Microsoft.BotBuilderSamples.Bots
