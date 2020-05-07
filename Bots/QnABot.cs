235d93a13c55c42385e869795a22cecd1e07d53d
projectvelox <43872288+projectvelox@users.noreply.github.com>
2020-05-08 05:46:47 +0800

    Update QnABot.cs

--- Bots/QnABot.cs
+++ Bots/QnABot.cs
@@ -97,7 +97,7 @@ namespace Microsoft.BotBuilderSamples.Bots
                 
 
 
-                IActivity replyActivity = MessageFactory.Text(dialogTask.FromResult.ToString());
+                IActivity replyActivity = MessageFactory.Text(dialogTask.ToString());
                 OmnichannelBotClient.BridgeBotMessage(replyActivity);
 
                 // Dialog.RunAsync(turnContext, ConversationState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
