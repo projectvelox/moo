c61bf826435878e242e76f23fd1eff5ad80b6e03
projectvelox <43872288+projectvelox@users.noreply.github.com>
2020-05-08 07:03:32 +0800

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -6,12 +6,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder.AI.QnA;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.BotFramework;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.BotBuilderSamples.Bots;
 using Microsoft.BotBuilderSamples.Dialog;
+using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using Microsoft.Extensions.Hosting;
 
 namespace Microsoft.BotBuilderSamples
 {
