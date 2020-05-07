4260723419b30c5cfddabc582a1933d9bf9c32ea
projectvelox <43872288+projectvelox@users.noreply.github.com>
2020-05-08 07:00:50 +0800

    startup

--- Startup.cs
+++ Startup.cs
@@ -48,15 +48,22 @@ namespace Microsoft.BotBuilderSamples
             services.AddSingleton<RootDialog>();
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-            services.AddTransient<IBot, QnABot<RootDialog>>();
+            //services.AddTransient<IBot, QnABot<RootDialog>>();
+
+            //Newly added
+            services.AddTransient<IBot, QnABot>();
+            services.AddControllers().AddNewtonsoftJson();
+            
+            // Add the HttpClientFactory to be used for the QnAMaker calls.
+            services.AddHttpClient();
+
+            // Create the credential provider to be used with the Bot Framework Adapter.
+            services.AddSingleton<ICredentialProvider, ConfigurationCredentialProvider>();
+
+            // Create the Bot Framework Adapter with error handling enabled. 
+            services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
 
 
-            /* services.AddSingleton(new QnAMakerEndpoint
-            {
-                KnowledgeBaseId = Configuration.GetValue<string>($"QnAKnowledgebaseId"),
-                EndpointKey = Configuration.GetValue<string>($"QnAAuthKey"),
-                Host = Configuration.GetValue<string>($"QnAEndpointHostName")
-            }); */
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
