c650a70e76b5498d1462ee8a2b7c124bc476f03e
projectvelox <43872288+projectvelox@users.noreply.github.com>
2020-05-08 08:54:24 +0800

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -47,6 +47,15 @@ namespace Microsoft.BotBuilderSamples
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
             services.AddTransient<IBot, QnABot<RootDialog>>();
+
+            services.AddControllers().AddNewtonsoftJson();
+
+            // Add the HttpClientFactory to be used for the QnAMaker calls.
+            services.AddHttpClient();
+
+            // Create the credential provider to be used with the Bot Framework Adapter.
+            services.AddSingleton<ICredentialProvider, ConfigurationCredentialProvider>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
