71a3bfd228d04e66d0ca702d0d7eb24b7b2e378e
projectvelox <43872288+projectvelox@users.noreply.github.com>
2020-05-08 08:50:29 +0800

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,14 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-using Microsoft.AspNetCore.Mvc;
+
 using Microsoft.AspNetCore.Builder;
-//using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
-using Microsoft.Bot.Builder.BotFramework;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-using Microsoft.Bot.Builder.AI.QnA;
 using Microsoft.BotBuilderSamples.Bots;
 using Microsoft.BotBuilderSamples.Dialog;
 using Microsoft.Extensions.Configuration;
@@ -50,15 +48,7 @@ namespace Microsoft.BotBuilderSamples
             services.AddSingleton<RootDialog>();
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-            services.AddTransient<IBot, QnABot<RootDialog>>();
-
-
-            /* services.AddSingleton(new QnAMakerEndpoint
-            {
-                KnowledgeBaseId = Configuration.GetValue<string>($"QnAKnowledgebaseId"),
-                EndpointKey = Configuration.GetValue<string>($"QnAAuthKey"),
-                Host = Configuration.GetValue<string>($"QnAEndpointHostName")
-            }); */
+            //services.AddTransient<IBot, QnABot<RootDialog>>();
 
             services.AddControllers().AddNewtonsoftJson();
 
@@ -72,7 +62,7 @@ namespace Microsoft.BotBuilderSamples
             services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-            //services.AddTransient<IBot, QnABot>();
+            services.AddTransient<IBot, QnABot>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -88,13 +78,13 @@ namespace Microsoft.BotBuilderSamples
             }
 
             app.UseDefaultFiles()
-                .UseStaticFiles()
-                .UseRouting()
-                .UseAuthorization()
-                .UseEndpoints(endpoints =>
-                {
-                    endpoints.MapControllers();
-                });
+                 .UseStaticFiles()
+                 .UseRouting()
+                 .UseAuthorization()
+                 .UseEndpoints(endpoints =>
+                 {
+                     endpoints.MapControllers();
+                 });
             app.UseStaticFiles();
 
             app.UseWebSockets();
