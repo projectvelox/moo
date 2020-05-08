335e60e05339c6775279253f02c5c4a2c5c8faf3
projectvelox <43872288+projectvelox@users.noreply.github.com>
2020-05-08 08:07:28 +0800

    reverting

--- Bots/QnABot.cs
+++ Bots/QnABot.cs
@@ -7,18 +7,15 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.AI.QnA;
 using Microsoft.Bot.Schema;
-<<<<<<< HEAD
 using EchoBot.OmniChannel;
-=======
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
->>>>>>> parent of aa6ee4c... Complete revamp
 
 namespace Microsoft.BotBuilderSamples
 {
     public class QnABot : ActivityHandler
     {
-<<<<<<< HEAD
         protected readonly BotState ConversationState;
         protected readonly Microsoft.Bot.Builder.Dialogs.Dialog Dialog;
         protected readonly BotState UserState;
@@ -29,17 +26,13 @@ namespace Microsoft.BotBuilderSamples
             ConversationState = conversationState;
             UserState = userState;
             Dialog = dialog;
-=======
         private readonly IConfiguration _configuration;
-        private readonly ILogger<QnABot> _logger;
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public QnABot(IConfiguration configuration, ILogger<QnABot> logger, IHttpClientFactory httpClientFactory)
+        public QnABot(IConfiguration configuration, IHttpClientFactory httpClientFactory)
         {
             _configuration = configuration;
-            _logger = logger;
             _httpClientFactory = httpClientFactory;
->>>>>>> parent of aa6ee4c... Complete revamp
         }
 
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
@@ -47,13 +40,17 @@ namespace Microsoft.BotBuilderSamples
             try
             {
 <<<<<<< HEAD
+
                 OmnichannelBotClient.BridgeBotMessage(turnContext.Activity);
                 await Dialog.RunAsync(turnContext, ConversationState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
                 await turnContext.SendActivityAsync(MessageFactory.Text($"Hello and welcome!"), cancellationToken);
 
-=======
+
                 var httpClient = _httpClientFactory.CreateClient();
 
+=======
+                var httpClient = _httpClientFactory.CreateClient();
+>>>>>>> parent of d51d80a... DUDE
                 var qnaMaker = new QnAMaker(new QnAMakerEndpoint
                 {
                     KnowledgeBaseId = _configuration["QnAKnowledgebaseId"],
@@ -69,7 +66,13 @@ namespace Microsoft.BotBuilderSamples
 
                 // The actual call to the QnA Maker service.
                 var response = await qnaMaker.GetAnswersAsync(turnContext, options);
+<<<<<<< HEAD
                 if (response != null && response.Length > 0)
+=======
+                await turnContext.SendActivityAsync(MessageFactory.Text(response[0].Answer), cancellationToken);
+
+                /*if (response != null && response.Length > 0)
+>>>>>>> parent of d51d80a... DUDE
                 {
                     await turnContext.SendActivityAsync(MessageFactory.Text(response[0].Answer), cancellationToken);
                 }
@@ -77,14 +80,12 @@ namespace Microsoft.BotBuilderSamples
                 {
                     await turnContext.SendActivityAsync(MessageFactory.Text("No QnA Maker answers were found."), cancellationToken);
                 }
->>>>>>> parent of aa6ee4c... Complete revamp
             }
             catch (Exception ex)
             {
                 await turnContext.SendActivityAsync(MessageFactory.Text(ex.ToString()), cancellationToken);
             }
         }
-<<<<<<< HEAD
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
@@ -97,7 +98,6 @@ namespace Microsoft.BotBuilderSamples
                 }
             }
         }
-=======
->>>>>>> parent of aa6ee4c... Complete revamp
+
     }
 }
