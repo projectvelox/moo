eba21e59be7f0751aaf21e227c9f58826b13617a
projectvelox <43872288+projectvelox@users.noreply.github.com>
2020-05-08 08:32:02 +0800

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -76,7 +76,7 @@ namespace Microsoft.BotBuilderSamples
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
