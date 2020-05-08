5ae343ed94aaaaa2926449d72f7a10b7b722f322
projectvelox <43872288+projectvelox@users.noreply.github.com>
2020-05-08 08:21:23 +0800

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,68 +1,19 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Bot.Builder.AI.QnA;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.BotFramework;
-using Microsoft.Bot.Connector.Authentication;
-
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
-using Microsoft.BotBuilderSamples.Bots;
-using Microsoft.BotBuilderSamples.Dialog;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
-
 
 namespace Microsoft.BotBuilderSamples
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-            // Create the Bot Framework Adapter with error handling enabled.
-            services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
-
-            // Create the bot services(QnA) as a singleton.
-            services.AddSingleton<IBotServices, BotServices>();
-
-            // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.)
-            services.AddSingleton<IStorage, MemoryStorage>();
-
-            // Create the User state. (Used in this bot's Dialog implementation.)
-            services.AddSingleton<UserState>();
-
-            // Create the Conversation state. (Used by the Dialog system itself.)
-            services.AddSingleton<ConversationState>();
-
-            // The Dialog that will be run by the bot.
-            services.AddSingleton<RootDialog>();
-
-            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-            //services.AddTransient<IBot, QnABot<RootDialog>>();
-
-
-            /* services.AddSingleton(new QnAMakerEndpoint
-            {
-                KnowledgeBaseId = Configuration.GetValue<string>($"QnAKnowledgebaseId"),
-                EndpointKey = Configuration.GetValue<string>($"QnAAuthKey"),
-                Host = Configuration.GetValue<string>($"QnAEndpointHostName")
-            }); */
-
             services.AddControllers().AddNewtonsoftJson();
 
             // Add the HttpClientFactory to be used for the QnAMaker calls.
@@ -79,15 +30,11 @@ namespace Microsoft.BotBuilderSamples
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
             }
 
             app.UseDefaultFiles()
@@ -99,10 +46,7 @@ namespace Microsoft.BotBuilderSamples
                     endpoints.MapControllers();
                 });
 
-            app.UseStaticFiles();
-
-            app.UseWebSockets();
-            app.UseMvc();
+            // app.UseHttpsRedirection();
         }
     }
 }
